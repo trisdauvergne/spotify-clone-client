b703a1e650ad047f73fcfd4ca9cf2d668aa93550
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-07-20 16:52:46 +0200

    Added a refresh useInterval for refreshing the access token before it expires

--- src/components/useAuth.js
+++ src/components/useAuth.js
@@ -6,13 +6,15 @@ const useAuth = (code) => {
   const [refreshToken, setRefreshToken] = useState();
   const [expiresIn, setExpiresIn] = useState();
 
+  // Hook to get the tokens from Spotify
   useEffect(() => {
-    console.log('code in useEffect in useAuth.js =', code);
     axios.post('http://localhost:3001/login', {
       code,
     })
     .then(res => {
-      console.log('data in useAuth.js', res.data);
+      setAccessToken(res.data.accessToken);
+      setRefreshToken(res.data.refreshToken);
+      setExpiresIn(res.data.expiresIn);
       window.history.pushState({}, '', '/');
     })
     .catch(err => {
@@ -21,11 +23,28 @@ const useAuth = (code) => {
     })
   }, [code]);
 
-  // return (
-  //   <div>
-      
-  //   </div>
-  // )
+  // Hook to refresh the expiresIn token
+  useEffect(() => {
+    if (!refreshToken | !expiresIn) return;
+
+    const interval = setInterval(() => {
+      axios.post('http://localhost:3001/refresh', {
+        refreshToken,
+      })
+      .then(res => {
+        setAccessToken(res.data.accessToken);
+        setExpiresIn(res.data.expiresIn);
+      })
+      .catch(err => {
+        console.log(err.response.data);
+        window.location = '/';
+      })
+    }, (expiresIn - 60) * 1000);
+
+    return () => clearInterval(interval);
+  }, [refreshToken, expiresIn])
+
+  return accessToken;
 }
 
 export default useAuth
